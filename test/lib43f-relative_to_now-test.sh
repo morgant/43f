20dd27bfcfd660cf5878b17c5d50d215e5cbea1a
Morgan Aldridge <morgant@makkintosshu.com>
2020-04-10 13:16:28 -0400

    Rename reldate() to relative_to_now()

--- test/lib43f-relative_to_now-test.sh
+++ test/lib43f-relative_to_now-test.sh
@@ -2,92 +2,92 @@
 
 source ./lib43f
 
-describe "lib43f reldate()"
+describe "lib43f relative_to_now()"
 
 it_errors_with_no_adjustment_string() {
-  ! reldate "" "+%d"
+  ! relative_to_now "" "+%d"
 }
 
 it_doesnt_error_with_a_valid_adjustment_string() {
-  reldate "+1d" "+%d"
+  relative_to_now "+1d" "+%d"
 }
 
 it_errors_without_a_leading_plus_symbol_on_format_string() {
-  ! reldate "+1d" "%d"
+  ! relative_to_now "+1d" "%d"
 }
 
 it_doesnt_error_with_a_leading_plus_symbol_on_format_string() {
-  reldate "+1d" "+%d"
+  relative_to_now "+1d" "+%d"
 }
 
 it_adjusts_the_day_by_zero_offset() {
   # without leading +/-
-  out="$(reldate "0d" "+%d")"
+  out="$(relative_to_now "0d" "+%d")"
   test "$out" = "$(date "+%d")"
 
   # with leading +
-  out="$(reldate "+0d" "+%d")"
+  out="$(relative_to_now "+0d" "+%d")"
   test "$out" = "$(date "+%d")"
 
   # with leading -
-  out="$(reldate "-0d" "+%d")"
+  out="$(relative_to_now "-0d" "+%d")"
   test "$out" = "$(date "+%d")"
 }
 
 it_adjusts_the_day_forward() {
-  out="$(reldate "+1d" "+%d")"
+  out="$(relative_to_now "+1d" "+%d")"
   test "$out" = "$(dadd -f "%d" "$(date "+%Y-%m-%d %H:%M:%S")" "+1d")"
 }
 
 it_adjusts_the_day_backward() {
-  out="$(reldate "-1d" "+%d")"
+  out="$(relative_to_now "-1d" "+%d")"
   test "$out" = "$(dadd -f "%d" "$(date "+%Y-%m-%d %H:%M:%S")" "-1d")"
 }
 
 it_adjusts_the_month_by_zero_offset() {
   # without leading +/-
-  out="$(reldate "0m" "+%m")"
+  out="$(relative_to_now "0m" "+%m")"
   test "$out" = "$(date "+%m")"
 
   # with leading +
-  out="$(reldate "+0m" "+%m")"
+  out="$(relative_to_now "+0m" "+%m")"
   test "$out" = "$(date "+%m")"
 
   # with leading -
-  out="$(reldate "-0m" "+%m")"
+  out="$(relative_to_now "-0m" "+%m")"
   test "$out" = "$(date "+%m")"
 }
 
 it_adjusts_the_month_forward() {
-  out="$(reldate "+1m" "+%m")"
+  out="$(relative_to_now "+1m" "+%m")"
   test "$out" = "$(dadd -f "%m" "$(date "+%Y-%m-%d %H:%M:%S")" "+1m")"
 }
 
 it_adjusts_the_month_backward() {
-  out="$(reldate "-1m" "+%m")"
+  out="$(relative_to_now "-1m" "+%m")"
   test "$out" = "$(dadd -f "%m" "$(date "+%Y-%m-%d %H:%M:%S")" "-1m")"
 }
 
 it_adjusts_the_year_by_zero_offset() {
   #without leading +/-
-  out="$(reldate "0y" "+%Y")"
+  out="$(relative_to_now "0y" "+%Y")"
   test "$out" = "$(date "+%Y")"
 
   # with leading +
-  out="$(reldate "+0y" "+%Y")"
+  out="$(relative_to_now "+0y" "+%Y")"
   test "$out" = "$(date "+%Y")"
 
   # with leading -
-  out="$(reldate "-0y" "+%Y")"
+  out="$(relative_to_now "-0y" "+%Y")"
   test "$out" = "$(date "+%Y")"
 }
 
 it_adjusts_the_year_forward() {
-  out="$(reldate "+1y" "+%Y")"
+  out="$(relative_to_now "+1y" "+%Y")"
   test "$out" = "$(dadd -f "%Y" "$(date "+%Y-%m-%d %H:%M:%S")" "+1y")"
 }
 
 it_adjusts_the_year_backward() {
-  out="$(reldate "-1y" "+%Y")"
+  out="$(relative_to_now "-1y" "+%Y")"
   test "$out" = "$(dadd -f "%Y" "$(date "+%Y-%m-%d %H:%M:%S")" "-1y")"
 }
